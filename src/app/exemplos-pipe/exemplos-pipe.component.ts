cc6ad138e050826bb8281aed1e22bc10886a9087
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-05-24 19:28:05 -0300

    initial commit

---
+++ src/app/exemplos-pipe/exemplos-pipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipe',
+  templateUrl: './exemplos-pipe.component.html',
+  styleUrls: ['./exemplos-pipe.component.css']
+})
+export class ExemplosPipeComponent {
+
+}
