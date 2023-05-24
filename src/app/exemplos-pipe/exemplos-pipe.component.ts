fda2c3b69a0526b539b2a7f24e57af5975d85f18
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-05-24 19:39:13 -0300

    Exemplo de pipes

--- src/app/exemplos-pipe/exemplos-pipe.component.ts
+++ src/app/exemplos-pipe/exemplos-pipe.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ExemplosPipeComponent {
 
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP'
+  }
 }
