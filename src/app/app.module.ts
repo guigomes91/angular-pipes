153e8da81104ce42d70e2ba4656658929a9053ec
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-05-25 21:02:35 -0300

    Change locale with services in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipeComponent } from './exemplos-pipe/exemplos-pipe.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -14,7 +19,14 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: any) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
